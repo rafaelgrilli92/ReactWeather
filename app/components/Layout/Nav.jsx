aaa276f02b97879f84493a143ab0a15ca549298d
Rafael Grilli <rafaelgrilli92@hotmail.com>
2016-09-17 22:28:04 +1000

    Fix some bugs

--- app/components/Layout/Nav.jsx
+++ app/components/Layout/Nav.jsx
@@ -3,11 +3,17 @@ var {Link, IndexLink} = require('react-router');
 
 var Nav = (props) => {
 	return (
-		<div>
-			<h2>Nav Component</h2>
-			<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-			<Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-			<Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+		<div className="top-bar">
+		  <div className="top-bar-left">
+		    <ul className="dropdown menu" data-dropdown-menu>
+		      <li className="menu-text">React Weather</li>
+		        <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+				<li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+				<li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+		    </ul>
+		  </div>
+		   <div class="top-bar-right">
+		   </div>
 		</div>
 	);
 }
