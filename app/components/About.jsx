db883789350e243c637f6310f6c4159811266e22
Rafael Grilli <rafaelgrilli92@hotmail.com>
2016-09-17 23:04:06 +1000

    Style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,7 +6,30 @@ var About = (props) => {
 	return (
 		<div>
 			<h1 className="text-center">About</h1>
-			<p>Welcome to about page!</p>
+			<p>
+				This is a weather application built using React. I've built this for study as well.
+			</p>
+			<p>
+				<h4>Repository</h4>
+				<a target="_blank" href="https://github.com/rafaelgrilli92/ReactWeather">
+				https://github.com/rafaelgrilli92/ReactWeather</a>
+			</p>
+			<p>
+				<b>Here are some of tools that I used:</b>
+				<ul>
+					<li><a target="_blank" href="https://facebook.github.io/react/">React</a></li>
+					<li><a target="_blank" href="https://nodejs.org/en/">NodeJS</a></li>
+					<li><a target="_blank" href="https://expressjs.com/">Express</a></li>
+					<li><a target="_blank" href="https://webpack.github.io/">Webpack</a></li>
+					<li><a target="_blank" href="https://babeljs.io/">Babel</a></li>
+					<li><a target="_blank" href="https://github.com/mzabriskie/axios">Axios</a></li>
+					<li><a target="_blank" href="https://jquery.com/">jQuery</a></li>
+					<li><a target="_blank" href="http://foundation.zurb.com/">Foundation</a></li>
+					<li><a target="_blank" href="http://openweathermap.org/api">OpenWeatherAPI</a></li>
+					<li><a target="_blank" href="https://github.com/">GIT</a></li>
+					<li><a target="_blank" href="https://heroku.com">Heroku</a></li>
+				</ul>
+			</p>
 		</div>
 	);
 };
