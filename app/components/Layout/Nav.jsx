b7723065d0291502c88b60333f228dba39e88718
Rafael Grilli <rafaelgrilli92@hotmail.com>
2016-09-17 22:33:36 +1000

    Style nav bar

--- app/components/Layout/Nav.jsx
+++ app/components/Layout/Nav.jsx
@@ -1,21 +1,37 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props) => {
-	return (
-		<div className="top-bar">
-		  <div className="top-bar-left">
-		    <ul className="dropdown menu" data-dropdown-menu>
-		      <li className="menu-text">React Weather</li>
-		        <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
-				<li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
-				<li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
-		    </ul>
-		  </div>
-		   <div class="top-bar-right">
-		   </div>
-		</div>
-	);
-}
+var Nav = React.createClass({
+	onSearch: function(e) {
+		e.preventDefault();
+		alert('Not yet wired up!');
+	},
+	render: function() {
+		return (
+			<div className="top-bar">
+			  <div className="top-bar-left">
+			    <ul className="dropdown menu" data-dropdown-menu>
+			      <li className="menu-text">React Weather App</li>
+			        <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+					<li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+					<li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+			    </ul>
+			  </div>
+			   <div className="top-bar-right">
+			   	<form onSubmit={this.onSearch}>
+			   		<ul className="menu">
+			   			<li>
+			   				<input type="search" placeholder="Search Weather..." />
+			   			</li>
+			   			<li>
+			   				<input type="submit" className="button" value="Get Weather" />
+			   			</li>
+			   		</ul>
+			   	</form>
+			   </div>
+			</div>
+		);
+	}
+})
 
 module.exports = Nav;
